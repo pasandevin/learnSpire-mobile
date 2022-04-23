9db6d7e3de9d29c4989002514653c4ffece588b8
pasandevin <jayawardene.pasandevin@gmail.com>
2022-04-23 19:25:29 +0530

    configured retrofit

---
+++ app/src/main/java/com/learnSpire/mobile/api/User.kt
@@ -0,0 +1,10 @@
+package com.learnSpire.mobile.api
+
+//fixme use real classes
+data class User(
+    val id: Number,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val website: String
+)
