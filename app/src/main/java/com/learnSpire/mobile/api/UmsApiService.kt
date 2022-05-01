f2b0eea7a606ea8967b463a6421c59db537604fc
Krishan Shamod <krishanshamod@gmail.com>
2022-05-01 16:59:11 +0530

    Added new API service

---
+++ app/src/main/java/com/learnSpire/mobile/api/UmsApiService.kt
@@ -0,0 +1,27 @@
+package com.learnSpire.mobile.api
+
+import com.learnSpire.mobile.models.SigninRequest
+import com.learnSpire.mobile.models.SigninResponse
+import com.learnSpire.mobile.models.SignupRequest
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UmsApiService {
+
+    @POST("/signin")
+    fun signIn(@Body signInRequest: SigninRequest): Call<SigninResponse>
+
+    companion object {
+        val API_URL = "https://ums.team8backend.tech"
+        fun create(): UmsApiService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(UmsApiService::class.java)
+        }
+    }
+}
