41e90136e037469fc799f2d7f8f8db3aaacca4ed
Krishan Shamod <krishanshamod@gmail.com>
2022-05-01 14:48:31 +0530

    Added login activity and fragments

---
+++ app/src/main/java/com/learnSpire/mobile/fragments/login/SignUpFragment.kt
@@ -0,0 +1,40 @@
+package com.learnSpire.mobile.fragments.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.learnSpire.mobile.R
+import com.learnSpire.mobile.databinding.FragmentSecondBinding
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
