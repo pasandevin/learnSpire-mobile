f49b2a18cdc43097ba60004a11b295246c926bfd
Krishan Shamod <krishanshamod@gmail.com>
2022-05-04 21:08:47 +0530

    Added logout functionality to the profile fragment

--- app/src/main/java/com/learnSpire/mobile/fragments/login/SignInFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/login/SignInFragment.kt
@@ -10,8 +10,10 @@ import com.learnSpire.mobile.R
 import com.learnSpire.mobile.api.UmsApiService
 import com.learnSpire.mobile.databinding.FragmentSigninBinding
 import androidx.navigation.fragment.findNavController
+import com.learnSpire.mobile.api.LmsApiService
 import com.learnSpire.mobile.models.SigninRequest
 import com.learnSpire.mobile.models.SigninResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,6 +24,7 @@ class SignInFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val umsApiService = UmsApiService.create()
+    private val lmsApiService = LmsApiService.create()
 
     private var userEmail: String = ""
     private var userPassword: String = ""
@@ -60,7 +63,7 @@ class SignInFragment : Fragment() {
                     // create new signIn request
                     var signInRequest: SigninRequest = SigninRequest(userEmail, userPassword)
 
-                    // call the api
+                    // call the signIn api
                     var signInResponse = umsApiService.signIn(signInRequest)
 
                     signInResponse.enqueue(object: Callback<SigninResponse> {
@@ -75,6 +78,19 @@ class SignInFragment : Fragment() {
                                         putString("token", it.token)
                                     }.apply()
 
+                                    // call the add user api
+                                    var addUserResponse = lmsApiService.addUser()
+
+                                    addUserResponse.enqueue(object: Callback<ResponseBody> {
+                                        override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+
+                                        }
+
+                                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                                            println("Failed to add user")
+                                        }
+                                    })
+
                                     // navigate to the home menu activity
                                     findNavController().navigate(R.id.action_SignInFragment_to_MenuActivity)
 
@@ -85,11 +101,9 @@ class SignInFragment : Fragment() {
                         }
 
                         override fun onFailure(call: Call<SigninResponse>, t: Throwable) {
-                            // will update in the future
                             println("Sign in failed")
                         }
                     })
-
                 }
             }
         }
