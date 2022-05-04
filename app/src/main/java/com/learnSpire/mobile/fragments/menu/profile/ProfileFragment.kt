d1d779f805d6af382009cec867349e3835b74252
Krishan Shamod <krishanshamod@gmail.com>
2022-05-04 21:09:17 +0530

    Updated the signin functionality

--- app/src/main/java/com/learnSpire/mobile/fragments/menu/profile/ProfileFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/profile/ProfileFragment.kt
@@ -1,39 +1,42 @@
 package com.learnSpire.mobile.fragments.menu.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.learnSpire.mobile.databinding.FragmentProfileBinding
-import com.learnSpire.mobile.fragments.menu.browse.ProfileViewModel
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
+    ): View? {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
 
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logOutButton.setOnClickListener {
+
+            // Initialized Shared Preferences
+            var sharedPreferences = requireContext()
+                .getSharedPreferences("SharedPrefFile", Context.MODE_PRIVATE)
+
+            // Clearing Shared Preferences
+            sharedPreferences.edit().clear().commit()
+
+            activity?.finish()
         }
-        return root
     }
 
     override fun onDestroyView() {
