2f187eae599581d0f0d938bd87b16e1c20e8c428
Krishan Shamod <krishanshamod@gmail.com>
2022-05-05 12:15:06 +0530

    Added recyclerview to the course fragment

--- app/src/main/java/com/learnSpire/mobile/fragments/menu/courses/CoursesFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/courses/CoursesFragment.kt
@@ -5,33 +5,64 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.learnSpire.mobile.adapters.EnrolledCoursesAdapter
+import com.learnSpire.mobile.api.LmsApiService
 import com.learnSpire.mobile.databinding.FragmentCoursesBinding
+import com.learnSpire.mobile.models.Course
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CoursesFragment : Fragment() {
 
     private var _binding: FragmentCoursesBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val lmsApiService = LmsApiService.create()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val coursesViewModel =
-            ViewModelProvider(this).get(CoursesViewModel::class.java)
 
         _binding = FragmentCoursesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
 
-//        val textView: TextView = binding.textHome
-        coursesViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-        }
-        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var enrolledCoursesList = ArrayList<Course>()
+
+        // call the get enrolled courses api
+        var getEnrolledCoursesResponse = lmsApiService.getEnrolledCourses()
+
+        getEnrolledCoursesResponse.enqueue(object: Callback<List<Course>> {
+            override fun onResponse(call: Call<List<Course>>, response: Response<List<Course>>) {
+                val body = response.body()
+
+                body.let {
+                    if (it != null) {
+                        enrolledCoursesList = it as ArrayList<Course>
+
+                        // set recycler view
+                        val recyclerView = binding.recyclerviewEnrolledCourses
+                        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+                        // set adapter
+                        val adapter = EnrolledCoursesAdapter(enrolledCoursesList)
+                        recyclerView.adapter = adapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Course>>, t: Throwable) {
+                println("Get Enrolled Courses failed")
+            }
+        })
     }
 
     override fun onDestroyView() {
