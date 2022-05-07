22ff17cbf0ee31e3222ba5d57a6c5d15f313856f
Krishan Shamod <krishanshamod@gmail.com>
2022-05-07 18:30:30 +0530

    Refactored the code

--- app/src/main/java/com/learnSpire/mobile/adapters/AvailableCoursesAdapter.kt
+++ app/src/main/java/com/learnSpire/mobile/adapters/AvailableCoursesAdapter.kt
@@ -1,6 +1,5 @@
 package com.learnSpire.mobile.adapters
 
-import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/learnSpire/mobile/fragments/menu/BrowseFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/BrowseFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.learnSpire.mobile.adapters.AvailableCoursesAdapter
-import com.learnSpire.mobile.adapters.CoursesAdapter
 import com.learnSpire.mobile.api.LmsApiService
 import com.learnSpire.mobile.databinding.FragmentBrowseBinding
 import com.learnSpire.mobile.models.Course
