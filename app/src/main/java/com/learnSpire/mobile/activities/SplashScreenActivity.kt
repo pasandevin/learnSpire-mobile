9124e154caf7332833697ed61a1456d9a687d8e5
Krishan Shamod <krishanshamod@gmail.com>
2022-05-07 09:50:20 +0530

    Added splash screen

---
+++ app/src/main/java/com/learnSpire/mobile/activities/SplashScreenActivity.kt
@@ -0,0 +1,39 @@
+package com.learnSpire.mobile.activities
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.learnSpire.mobile.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Initialized Shared Preferences
+        var sharedPreferences = getSharedPreferences("SharedPrefFile", Context.MODE_PRIVATE)
+
+        // get token value from shared preferences
+        var token = sharedPreferences.getString("token", "")
+
+        // Initialize the image view
+        var imageLogo = findViewById<ImageView>(R.id.image_logo)
+
+        // Start the animation
+        imageLogo.alpha = 0f
+
+        imageLogo.animate().alpha(1f).setDuration(1500).withEndAction {
+
+            // check if token is empty or not and navigate to appropriate activity
+            if (token == "") {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
--- app/src/main/java/com/learnSpire/mobile/fragments/menu/ProfileFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/ProfileFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.learnSpire.mobile.R
 import com.learnSpire.mobile.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -35,7 +37,8 @@ class ProfileFragment : Fragment() {
             // Clearing Shared Preferences
             sharedPreferences.edit().clear().commit()
 
-            activity?.finish()
+            // navigate to the home login activity
+            findNavController().navigate(R.id.action_ProfileFragment_to_LoginActivity)
         }
     }
 
