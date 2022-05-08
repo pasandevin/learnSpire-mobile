7347e28e6e3b8edba26aca77d8c623a2c1a4028f
Krishan Shamod <krishanshamod@gmail.com>
2022-05-09 00:37:51 +0530

    Added the profile fragment functionality

--- app/src/main/java/com/learnSpire/mobile/fragments/menu/ProfileFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/ProfileFragment.kt
@@ -7,13 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.learnSpire.mobile.R
+import com.learnSpire.mobile.api.LmsApiService
 import com.learnSpire.mobile.databinding.FragmentProfileBinding
+import com.learnSpire.mobile.models.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val lmsApiService = LmsApiService.create()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +36,42 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // call the get user api
+        var getUserResponse = lmsApiService.getUser()
+
+        getUserResponse.enqueue(object: Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                val body = response.body()
+
+                body.let {
+                    if (it != null) {
+
+                        val user = it
+
+                        // generate random color
+                        val generator = ColorGenerator.MATERIAL
+
+                        // get the first letter of the name
+                        val letter = user.firstName[0].toString()
+
+                        // generate thumbnail image
+                        val drawable = TextDrawable.builder()
+                            .buildRound(letter, generator.getRandomColor())
+
+                        // set data to views
+                        binding.nameViewEdit.text = user.firstName + " " + user.lastName
+                        binding.emailViewEdit.text = user.email
+                        binding.roleViewEdit.text = user.role
+                        binding.profilePictureImageView.setImageDrawable(drawable)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                println("Get User failed")
+            }
+        })
 
         binding.logOutButton.setOnClickListener {
 
