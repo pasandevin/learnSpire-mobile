09fbaf2349500ef43f77a18b1f97b09dfc8c6968
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-09 02:25:23 +0530

    set notification adapter

---
+++ app/src/main/java/com/learnSpire/mobile/adapters/NotificationsAdapter.kt
@@ -0,0 +1,76 @@
+package com.learnSpire.mobile.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
+import com.learnSpire.mobile.R
+import com.learnSpire.mobile.models.Course
+import com.learnSpire.mobile.models.GetNotificationsResponse
+
+class NotificationsAdapter(private val notificationsList: List<GetNotificationsResponse>): RecyclerView.Adapter<NotificationsAdapter.ViewHolder>() {
+
+    companion object {
+        public var courseId = ""
+        public var courseName = ""
+        public var title = ""
+    }
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_notifications, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val notification: GetNotificationsResponse = notificationsList[position]
+
+        // set the course name
+        holder.courseNameText.text = notification.courseName
+
+        // set the notification title
+        holder.courseTitleText.text = notification.title
+
+//        // generate random color
+//        val generator = ColorGenerator.MATERIAL
+//
+//        // generate thumbnail image
+//        val drawable = TextDrawable.builder().buildRect("", generator.getRandomColor())
+
+        // set the image
+//        holder.imageView.setImageDrawable(drawable)
+
+        // navigate to the notification content fragment and transfer data bundle
+        holder.cardView.setOnClickListener {
+
+            // set the course id and notification title to access from other fragments or activities
+            courseId = notification.courseId
+            title = notification.title
+
+
+            // navigate to the course content activity
+//            it.findNavController().navigate(R.id.action_NotificationsFragment_to_NotificationContentActivity)
+        }
+    }
+
+    //number of the items in the list
+    override fun getItemCount(): Int {
+        return notificationsList.size
+    }
+
+    // Holds the views for adding it to text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val cardView: CardView = itemView.findViewById(R.id.cardview_notifications)
+        val courseNameText: TextView = itemView.findViewById(R.id.text_course_name)
+        val courseTitleText: TextView = itemView.findViewById(R.id.text_notification_title)
+    }
+}
--- app/src/main/java/com/learnSpire/mobile/fragments/menu/NotificationsFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/NotificationsFragment.kt
@@ -7,13 +7,23 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.learnSpire.mobile.adapters.EnrolledCoursesAdapter
+import com.learnSpire.mobile.adapters.NotificationsAdapter
+import com.learnSpire.mobile.api.LmsApiService
 import com.learnSpire.mobile.databinding.FragmentBrowseBinding
 import com.learnSpire.mobile.databinding.FragmentNotificationsBinding
+import com.learnSpire.mobile.models.Course
+import com.learnSpire.mobile.models.GetNotificationsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
     private val binding get() = _binding!!
+    private val lmsApiService = LmsApiService.create()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +34,39 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var notificationsList = ArrayList<GetNotificationsResponse>()
+
+        // call the get enrolled courses api
+        var getNotificationsResponse = lmsApiService.getAllNotifications()
+
+        getNotificationsResponse.enqueue(object: Callback<List<GetNotificationsResponse>> {
+            override fun onResponse(call: Call<List<GetNotificationsResponse>>, response: Response<List<GetNotificationsResponse>>) {
+                val body = response.body()
+
+                body.let {
+                    if (it != null) {
+                        notificationsList = it as ArrayList<GetNotificationsResponse>
+
+                        // set recycler view
+                        val recyclerView = binding.recyclerviewNotifications
+                        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+                        // set adapter
+                        val adapter = NotificationsAdapter(notificationsList)
+                        recyclerView.adapter = adapter
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<GetNotificationsResponse>>, t: Throwable) {
+                println("Get All Notifications failed")
+            }
+        })
     }
 
     override fun onDestroyView() {
