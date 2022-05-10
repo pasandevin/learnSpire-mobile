7cf45357ead85834c04cbb522bcea01b600f2c80
Krishan Shamod <krishanshamod@gmail.com>
2022-05-10 12:02:01 +0530

    Added lecturer menu activity

---
+++ app/src/main/java/com/learnSpire/mobile/activities/LecturerMenuActivity.kt
@@ -0,0 +1,35 @@
+package com.learnSpire.mobile.activities
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.learnSpire.mobile.R
+import com.learnSpire.mobile.databinding.ActivityLecturerMenuBinding
+
+class LecturerMenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLecturerMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLecturerMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_lecturer_menu)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
