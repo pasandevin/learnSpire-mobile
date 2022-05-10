4b0886fa88c0fa919f587f0c5abdad31a4c46d0e
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-11 02:39:24 +0530

    configured course adding functionality

--- app/src/main/java/com/learnSpire/mobile/activities/AddContentActivity.kt
+++ app/src/main/java/com/learnSpire/mobile/activities/AddContentActivity.kt
@@ -3,25 +3,10 @@ package com.learnSpire.mobile.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.learnSpire.mobile.R
-import com.learnSpire.mobile.databinding.ActivityAddCourseBinding
-import com.learnSpire.mobile.databinding.ActivityLecturerMenuBinding
 
 class AddContentActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityAddCourseBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityAddCourseBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.buttonSubmit.setOnClickListener {
-            var courseId = binding.editTextCourseId.text.toString()
-            var courseName = binding.editTextCourseName.text.toString()
-
-        }
-
-
+        setContentView(R.layout.activity_add_content)
     }
 }
