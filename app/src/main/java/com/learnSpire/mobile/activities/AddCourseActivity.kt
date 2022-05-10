d2e2f2b46e59d8e34c334c00d9c42c8650034564
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-11 00:20:44 +0530

    updated add course ui

--- app/src/main/java/com/learnSpire/mobile/activities/AddCourseActivity.kt
+++ app/src/main/java/com/learnSpire/mobile/activities/AddCourseActivity.kt
@@ -1,10 +1,16 @@
 package com.learnSpire.mobile.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+
+import androidx.appcompat.app.AppCompatActivity
 import com.learnSpire.mobile.R
 
 class AddCourseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_course)
