ae1766662c39f283c4e60675dac5a4f07fa0e998
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-10 20:26:42 +0530

    updated section adding ui

--- app/src/main/java/com/learnSpire/mobile/fragments/menu/AddFragment.kt
+++ app/src/main/java/com/learnSpire/mobile/fragments/menu/AddFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AddSectionFragment.newInstance] factory method to
+ * Use the [AddFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddSectionFragment : Fragment() {
+class AddFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -45,12 +45,12 @@ class AddSectionFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment AddSectionFragment.
+         * @return A new instance of fragment AddFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AddSectionFragment().apply {
+            AddFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
