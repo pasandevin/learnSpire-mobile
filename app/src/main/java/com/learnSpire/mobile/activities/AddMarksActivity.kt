e1a800c92ee56dc2f7923424a9f0dcb80dee7c59
pasandevin <jayawardene.pasandevin@gmail.com>
2022-05-13 00:46:12 +0530

    configured viewBindings for addMarksActivity

--- app/src/main/java/com/learnSpire/mobile/activities/AddMarksActivity.kt
+++ app/src/main/java/com/learnSpire/mobile/activities/AddMarksActivity.kt
@@ -3,10 +3,18 @@ package com.learnSpire.mobile.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.learnSpire.mobile.R
+import com.learnSpire.mobile.databinding.ActivityAddMarksBinding
+import com.learnSpire.mobile.databinding.ActivityLecturerCourseContentBinding
+import com.learnSpire.mobile.databinding.ActivityLecturerMenuBinding
 
 class AddMarksActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddMarksBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_marks)
+
+        binding = ActivityAddMarksBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
