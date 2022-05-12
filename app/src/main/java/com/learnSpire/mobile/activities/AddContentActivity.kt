eb57947990560ddf6fd66475f53c521614b9c685
Krishan Shamod <krishanshamod@gmail.com>
2022-05-12 15:39:01 +0530

    Added the add content functionality

--- app/src/main/java/com/learnSpire/mobile/activities/AddContentActivity.kt
+++ app/src/main/java/com/learnSpire/mobile/activities/AddContentActivity.kt
@@ -1,12 +1,81 @@
 package com.learnSpire.mobile.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.learnSpire.mobile.R
+import android.widget.Toast
+import com.learnSpire.mobile.adapters.EnrolledCoursesAdapter
+import com.learnSpire.mobile.api.LmsApiService
+import com.learnSpire.mobile.databinding.ActivityAddContentBinding
+import com.learnSpire.mobile.models.AddContentRequest
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddContentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddContentBinding
+
+    private val lmsApiService = LmsApiService.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_content)
+        binding = ActivityAddContentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val courseId = EnrolledCoursesAdapter.courseId
+        val courseName = EnrolledCoursesAdapter.courseName
+
+        title = "Add Content: $courseName"
+
+        binding.buttonSubmit.setOnClickListener {
+            var contentTitle = binding.editTextContentTitle.text.toString()
+            var content = binding.editTextContent.text.toString()
+
+            // Check if the fields are empty or not
+            if (contentTitle.isEmpty() || content.isEmpty()) {
+
+                binding.errorTextView.text = "Please fill all the fields"
+
+            } else {
+
+                var courseContent = AddContentRequest(courseId, contentTitle, content)
+
+                // call the add course api
+                var addContentResponse = lmsApiService.addContent(courseContent)
+
+                addContentResponse.enqueue(object : Callback<ResponseBody> {
+
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        if (response.isSuccessful) {
+
+                            // show success message
+                            Toast.makeText(
+                                this@AddContentActivity,
+                                "Content Added Successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            // go to the course list fragment
+                            Intent(this@AddContentActivity, LecturerMenuActivity::class.java).also {
+                                startActivity(it)
+                            }
+
+                        } else {
+                            // show error message
+                            println("Failed to add content")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                        println("Failed to add content")
+                    }
+                })
+            }
+        }
     }
 }
